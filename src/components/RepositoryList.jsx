892b10af6d51070a49e5c79473f8f5e9118c599c
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-05-21 11:50:15 -0300

    feat: first Commit

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,14 +1,17 @@
+import React, { useState } from "react";
 import { ReppositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
-const repository ={
+const Repository ={
     name: 'unform2',
     description: 'Forms is React',
     link: 'https://github.com/unform/unform'
 }
 
 function RepositoryList() {
+    const [] = useState<Repository>([]);
+
     return (
         <section className="repository-list">
             <h1>Lista de reporitório</h1>
