5d680728c1e6b40cf04b10d120a86a6af300558a
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-05-23 14:52:58 -0300

    feat: rocks / useEfect and useStatus

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
 import { ReppositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
 
 import '../styles/repositories.scss'
 
-const Repository ={
+const repository ={
     name: 'unform2',
     description: 'Forms is React',
     link: 'https://github.com/unform/unform'
 }
 
 function RepositoryList() {
-    const [] = useState<Repository>([]);
+    const [repository, setRepository] = useState<repository>([])
 
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepository(data))
+    })
     return (
         <section className="repository-list">
             <h1>Lista de reporitório</h1>
