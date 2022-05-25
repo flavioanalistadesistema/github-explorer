a8a984f6e85eb66b24dd5714f5280034cf29eca7
Flávio Santos <flavioanalistadesistema@gmail.com>
2022-05-25 11:53:36 -0300

    feat: utilizando Listagem

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,28 +3,28 @@ import { useState, useEffect } from "react";
 
 import '../styles/repositories.scss'
 
-const repository ={
+const repository = {
     name: 'unform2',
     description: 'Forms is React',
     link: 'https://github.com/unform/unform'
 }
 
 function RepositoryList() {
-    const [repository, setRepository] = useState<repository>([])
+    const [rep, setRep] = useState([]);
+    console.log(rep, 'rep');
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
-        .then(response => response.json())
-        .then(data => setRepository(data))
-    })
+            .then(response => response.json())
+            .then(data => setRep(data))
+    }, [])
+    
     return (
         <section className="repository-list">
             <h1>Lista de reporitório</h1>
 
             <ul>
-                <ReppositoryItem repository={repository}/>
-                <ReppositoryItem repository={repository}/>
-                <ReppositoryItem repository={repository}/>
+                <ReppositoryItem repository={rep} />
             </ul>
         </section>
     )
